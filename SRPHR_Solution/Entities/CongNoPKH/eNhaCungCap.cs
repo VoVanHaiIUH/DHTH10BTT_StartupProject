547fd5cb811d33b6b52de50d739946319b3cdbae
Võ Hồng Thái <thaivo181196@gmail.com>
2016-10-28 17:04:59 +0700

    xây dựng các class trong Entities và Business Logic

---
+++ SRPHR_Solution/Entities/CongNoPKH/eNhaCungCap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.CongNoPKH
+{
+    public class eNhaCungCap
+    {
+        public string maNCC { get; set; }
+        public string tenNCC { get; set; }
+        public string diaChi { get; set; }
+        public string sDT { get; set; }
+        public string email { get; set; }
+        public eNhaCungCap(string ma, string ten, string dc, string sdt, string email)
+        {
+            this.maNCC = ma;
+            this.tenNCC = ten;
+            this.diaChi = dc;
+            this.sDT = sdt;
+            this.email = email;
+        }
+        public override int GetHashCode()
+        {
+            return maNCC.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            eNhaCungCap ncc = (eNhaCungCap)obj;
+            return maNCC.ToLower().Equals(ncc.maNCC);
+        }
+    }
+}
