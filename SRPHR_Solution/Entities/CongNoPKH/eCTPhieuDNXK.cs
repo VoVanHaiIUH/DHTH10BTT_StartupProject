cc7d827141f00158918fccb8b255c70238a63499
junsu2404 <emcuangayhomqua2404@gmail.com>
2016-10-28 20:11:46 +0700

    Update eCTPhieuDNXK.cs

--- SRPHR_Solution/Entities/CongNoPKH/eCTPhieuDNXK.cs
+++ SRPHR_Solution/Entities/CongNoPKH/eCTPhieuDNXK.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,27 @@ namespace Entities.CongNoPKH
 {
     public class eCTPhieuDNXK
     {
+        public string soPDNXK { get; set; }
+        public string maSP { get; set; }
+        public int soluong { get; set; }
+        public string Ghichu { get; set; }
+        public eCTPhieuDNXK(string soPDNXK, string maSP, int soluong, string Ghichu)
+        {
+            this.soPDNXK = soPDNXK;
+            this.maSP = maSP;
+            this.soluong = soluong;
+            this.Ghichu = Ghichu;
+        }
+        public override int GetHashCode()
+        {
+            return soPDNXK.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            eCTPhieuDNXK ctpdnnk = (eCTPhieuDNXK)obj;
+            return ctpdnnk.Equals(ctpdnnk.soPDNXK);
+        }
     }
 }
+
+
