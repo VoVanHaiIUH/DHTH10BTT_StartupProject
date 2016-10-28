40d8283f8d7e62fad1aa6af42b9e2ba0474063c6
junsu2404 <emcuangayhomqua2404@gmail.com>
2016-10-28 20:08:53 +0700

    Update ePhieuDNNK.cs

--- SRPHR_Solution/Entities/CongNoPKH/ePhieuDNNK.cs
+++ SRPHR_Solution/Entities/CongNoPKH/ePhieuDNNK.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,27 @@ namespace Entities.CongNoPKH
 {
     public class ePhieuDNNK
     {
+        public string soPDNNK { get; set; }
+        public string maNV { get; set; }
+        public string ngayLapphieu { get; set; }
+        public string maKho { get; set; }
+        public ePhieuDNNK(string soPDNNK, string maNV, string ngayLapphieu, string maKho)
+        {
+            this.soPDNNK = soPDNNK;
+            this.maNV = maNV;
+            this.ngayLapphieu = ngayLapphieu;
+            this.maKho = maKho;
+        }
+        public override int GetHashCode()
+        {
+            return soPDNNK.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            ePhieuDNNK pdnnk = (ePhieuDNNK)obj;
+            return pdnnk.Equals(pdnnk.soPDNNK);
+        }
     }
 }
+
+
