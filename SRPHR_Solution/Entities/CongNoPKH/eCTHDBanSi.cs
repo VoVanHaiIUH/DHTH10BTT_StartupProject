ef35c938ba165cebc44b941c8ce5039afe28f8d5
thang <thang@THANG-PC>
2016-10-29 12:05:21 +0700

    commit

--- SRPHR_Solution/Entities/CongNoPKH/eCTHDBanSi.cs
+++ SRPHR_Solution/Entities/CongNoPKH/eCTHDBanSi.cs
@@ -8,5 +8,28 @@ namespace Entities.CongNoPKH
 {
     public class eCTHDBanSi
     {
+        public string maHD { get; set; }
+        public string maSP { get; set; }
+        public float soluong { get; set; }
+        public string dongiaBan { get; set; }
+        public string ghiChu { get; set; }
+        public eCTHDBanSi(string maHD, string maSP, float soluong, string dongiaBan, string ghiChu)
+        {
+            this.maHD = maHD;
+            this.maSP = maSP;
+            this.soluong = soluong;
+            this.dongiaBan = dongiaBan;
+            this.ghiChu = ghiChu;
+        }
+        public override int GetHashCode()
+        {
+            return maHD.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            eNhaCungCap ncc = (eNhaCungCap)obj;
+            return maHD.ToLower().Equals(ncc.maNCC);
+        }
     }
+
 }
--- SRPHR_Solution/Entities/CongNoPKH/eHDMuaHangNCC.cs
+++ SRPHR_Solution/Entities/CongNoPKH/eHDMuaHangNCC.cs
@@ -8,5 +8,30 @@ namespace Entities.CongNoPKH
 {
     public class eHDMuaHangNCC
     {
+        public string maHD { get; set; }
+        public string tenHD { get; set; }
+        public string ngayLap { get; set; }
+        public string maNCC { get; set; }
+        public string maNV { get; set; }
+        public string noidung { get; set; }
+        
+        public eHDMuaHangNCC(string maHD, string ngaylap, string maKH, string maNV, string ghichu, string tenHD)
+        {
+            this.maHD = maHD;
+            this.ngayLap = ngaylap;
+            this.maNCC = maNCC;
+            this.maNV = maNV;
+            this.noidung = noidung;
+            this.tenHD = tenHD;
+        }
+        public override int GetHashCode()
+        {
+            return maKH.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            eNhaCungCap ncc = (eNhaCungCap)obj;
+            return maKH.ToLower().Equals(ncc.maNCC);
+        }
     }
 }
