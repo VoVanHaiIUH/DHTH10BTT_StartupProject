0aedbef271a6cf516513d6e274dae30435185640
NguyenVanThuyet <thuyetnv1995@gmail.com>
2016-10-30 15:18:15 +0700

    commit

--- SRPHR_Solution/Entities/CongNoPKH/eCTCongNoNCC.cs
+++ SRPHR_Solution/Entities/CongNoPKH/eCTCongNoNCC.cs
@@ -8,5 +8,26 @@ namespace Entities.CongNoPKH
 {
     public class eCTCongNoNCC
     {
+         public string maNCC { get; set; }
+        public string soTien { get; set; }
+        public string maHD { get; set; }
+        public string ghiChu { get; set; }
+        public eCTCongNoNCC(string mancc, string sotien, string mahd, string ghichu)
+        {
+            this.maNCC = mancc;
+            this.maHD = mahd;
+            this.soTien = sotien;
+            this.ghiChu = ghichu;
+
+        }
+        public override bool Equals(object obj)
+        {
+            eCTCongNoNCC encc = (eCTCongNoNCC)obj;
+            return encc.Equals(encc.maNCC);
+        }
+        public override int GetHashCode()
+        {
+            return maNCC.GetHashCode();
+        }
     }
 }
