485a5c14e47661b2b7992dd01327728968146d28
TranHuynhNhatTan <omega6192000@gmail.com>
2016-10-30 15:08:51 +0700

    Update eCTKhuyenMai.cs va eKhuyenMai

--- SRPHR_Solution/Entities/CongNoPKH/eKhuyenMai.cs
+++ SRPHR_Solution/Entities/CongNoPKH/eKhuyenMai.cs
@@ -8,5 +8,28 @@ namespace Entities.CongNoPKH
 {
     public class eKhuyenMai
     {
+        public string msDotKM { get; set; }
+        public string tenDotKM { get; set; }
+        public DateTime ngayBD { get; set; }
+        public DateTime ngayKT { get; set; }
+        public string ghiChu { get; set; }
+
+        public eKhuyenMai(string msDotKM, string maSP, double phantramKM, string ghiChu)
+        {
+            this.msDotKM = msDotKM;
+            this.tenDotKM = tenDotKM;
+            this.ngayBD = ngayBD;
+            this.ngayKT = ngayKT;
+            this.ghiChu = ghiChu;
+        }
+        public override int GetHashCode()
+        {
+            return msDotKM.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            eKhuyenMai km = (eKhuyenMai)obj;
+            return km.Equals(km.msDotKM);
+        }
     }
 }
