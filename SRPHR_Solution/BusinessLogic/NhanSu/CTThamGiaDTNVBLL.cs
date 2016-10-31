d2ea7e708ce267e17740640f0d3e686fce5bf736
huynhdien <dienhuynh880@gmail.com>
2016-10-31 16:46:22 +0700

    CTThamGiaDTNV CTKyLuatNV LichSuLamViec update Entities vs BLL

--- SRPHR_Solution/BusinessLogic/NhanSu/CTThamGiaDTNVBLL.cs
+++ SRPHR_Solution/BusinessLogic/NhanSu/CTThamGiaDTNVBLL.cs
@@ -10,6 +10,57 @@ namespace BusinessLogic.NhanSu
 {
     public class CTThamGiaDTNVBLL
     {
+        //SRPHRDataContext DB = new SRPHRDataContext();
+        //public bool Add(CTThamGiaDTNV newCTTGDT)
+        //{
+        //    try
+        //    {
+        //        Tbl_CTThamGiaDTNV newRecord = DB.Tbl_CTThamGiaDTNVs.Single(record => record.maNV == newCTTGDT.MaNV && record.maKhoaDT == newCTTGDT.MaKhoaDT);
+        //        newRecord.maNV = newCTTGDT.MaNV;
+        //        newRecord.maKhoaDT = newCTTGDT.MaKhoaDT;
+        //        newRecord.nhanXet = newCTTGDT.NhanXet;
+        //        newRecord.ketQua = newCTTGDT.KetQua;
+        //        DB.Tbl_CTThamGiaDTNVs.InsertOnSubmit(newRecord);
+        //        DB.SubmitChanges();
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+        //}
+        //public bool Delete(string deleteID)
+        //{
+        //    try
+        //    {
+        //        Tbl_CTThamGiaDTNV deleteRecord = DB.Tbl_CTThamGiaDTNVs.Single(record => record.maNV == deleteID && record.maKhoaDT == deleteID);
+        //        DB.Tbl_CTThamGiaDTNVs.DeleteOnSubmit(deleteRecord);
+        //        DB.SubmitChanges();
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+        //}
+        //public bool UpDate(CTThamGiaDTNV updateCTDTNV)
+        //{
+        //    try
+        //    {
+        //        Tbl_CTThamGiaDTNV updateRecord = DB.Tbl_CTThamGiaDTNVs.Single(record => record.maNV == updateCTDTNV.MaNV && record.maKhoaDT == updateCTDTNV.MaKhoaDT);
+        //        updateRecord.maNV = updateCTDTNV.MaNV;
+        //        updateRecord.maKhoaDT = updateCTDTNV.MaKhoaDT;
+        //        updateRecord.nhanXet = updateCTDTNV.NhanXet;
+        //        updateRecord.ketQua = updateCTDTNV.KetQua;
+        //        DB.SubmitChanges();
+        //        return true;
+            
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+        //}
         SRPHRDataContext DB = new SRPHRDataContext();
         public bool Add(CTThamGiaDTNV newCTTGDT)
         {
@@ -54,7 +105,7 @@ namespace BusinessLogic.NhanSu
                 updateRecord.ketQua = updateCTDTNV.KetQua;
                 DB.SubmitChanges();
                 return true;
-            
+
             }
             catch
             {
--- SRPHR_Solution/Entities/NhanSu/CTThamGiaDTNV.cs
+++ SRPHR_Solution/Entities/NhanSu/CTThamGiaDTNV.cs
@@ -8,6 +8,63 @@ namespace Entities.NhanSu
 {
     public class CTThamGiaDTNV
     {
+        //#region property
+        //private string _maNV;
+        //private string _maKhoaDT;
+        //private string _ketQua;
+        //private string _nhanXet;
+        //#endregion
+
+        //#region property get set
+        //public string NhanXet
+        //{
+        //    get { return _nhanXet; }
+        //    set { _nhanXet = value; }
+        //}
+
+        //public string KetQua
+        //{
+        //    get { return _ketQua; }
+        //    set { _ketQua = value; }
+        //}
+
+        //public string MaKhoaDT
+        //{
+        //    get { return _maKhoaDT; }
+        //    set { _maKhoaDT = value; }
+        //}
+
+        //public string MaNV
+        //{
+        //    get { return _maNV; }
+        //    set { _maNV = value; }
+        //}
+        //#endregion
+        //#region constructor
+        //public CTThamGiaDTNV(string maNV,string maKhoaDT,string ketQua,string nhanXet)
+        //{
+        //    MaNV = maNV;
+        //    MaKhoaDT = maKhoaDT;
+        //    KetQua = ketQua;
+        //    NhanXet = nhanXet;
+        //}
+        //#endregion
+
+        //#region Methods manhanvien
+        //public override bool Equals(Object newNV)
+        //{
+        //    CTThamGiaDTNV nv = (CTThamGiaDTNV)newNV;
+        //    return this.MaNV.ToLower().Equals(nv.MaNV.ToLower());
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return base.GetHashCode();
+        //}
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //}
+        //#endregion
         #region property
         private string _maNV;
         private string _maKhoaDT;
@@ -41,7 +98,7 @@ namespace Entities.NhanSu
         }
         #endregion
         #region constructor
-        public CTThamGiaDTNV(string maNV,string maKhoaDT,string ketQua,string nhanXet)
+        public CTThamGiaDTNV(string maNV, string maKhoaDT, string ketQua, string nhanXet)
         {
             MaNV = maNV;
             MaKhoaDT = maKhoaDT;
