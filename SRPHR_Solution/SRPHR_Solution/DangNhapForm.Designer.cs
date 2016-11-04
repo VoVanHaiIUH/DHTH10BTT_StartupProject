a59a34d61386928d0223a3e8b331bac3f3a2e0df
keal0129 <keal0129@gmail.com>
2016-11-04 23:38:10 +0700

    Update GUI PhanQuyen

--- SRPHR_Solution/SRPHR_Solution/DangNhapForm.Designer.cs
+++ SRPHR_Solution/SRPHR_Solution/DangNhapForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SRPHR_Solution
 {
-    partial class Form1
+    partial class FormDangnhap
     {
         /// <summary>
         /// Required designer variable.
@@ -31,74 +31,86 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnlogin = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.btndangki = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 64);
+            this.textBox1.Location = new System.Drawing.Point(109, 54);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 38);
+            this.textBox2.Location = new System.Drawing.Point(109, 28);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 41);
+            this.label1.Location = new System.Drawing.Point(12, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(79, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Tên tài khoản :";
             // 
-            // button1
+            // btnlogin
             // 
-            this.button1.Location = new System.Drawing.Point(43, 104);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnlogin.Location = new System.Drawing.Point(25, 96);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "Đăng nhập";
+            this.btnlogin.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 67);
+            this.label2.Location = new System.Drawing.Point(39, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "Mật khẩu :";
             // 
-            // button2
+            // btnexit
             // 
-            this.button2.Location = new System.Drawing.Point(153, 104);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnexit.Location = new System.Drawing.Point(187, 96);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(75, 23);
+            this.btnexit.TabIndex = 5;
+            this.btnexit.Text = "Thoát";
+            this.btnexit.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // btndangki
+            // 
+            this.btndangki.Location = new System.Drawing.Point(106, 96);
+            this.btndangki.Name = "btndangki";
+            this.btndangki.Size = new System.Drawing.Size(75, 23);
+            this.btndangki.TabIndex = 6;
+            this.btndangki.Text = "Đăng Kí ";
+            this.btndangki.UseVisualStyleBackColor = true;
+            // 
+            // FormDangnhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 149);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(301, 151);
+            this.Controls.Add(this.btndangki);
+            this.Controls.Add(this.btnexit);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnlogin);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "FormDangnhap";
+            this.Text = "Đăng Nhập";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,9 +121,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnlogin;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Button btndangki;
     }
 }
 
--- SRPHR_Solution/SRPHR_Solution/DangNhapForm.cs
+++ SRPHR_Solution/SRPHR_Solution/DangNhapForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SRPHR_Solution
 {
-    public partial class Form1 : Form
+    public partial class FormDangnhap : Form
     {
-        public Form1()
+        public FormDangnhap()
         {
             InitializeComponent();
         }
