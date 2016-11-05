0873dd089c6f521d584eb447e3a855817a69821b
nguyenanhduy1412 <nguyenanhduy1412@gmail.com>
2016-11-05 09:19:33 +0700

    sua loi PhongBanBLL

--- SRPHR_Solution/BusinessLogic/NhanSu/MyConvert.cs
+++ SRPHR_Solution/BusinessLogic/NhanSu/MyConvert.cs
@@ -8,139 +8,99 @@ using Entities.NhanSu;
 using DataAccess;
 namespace BusinessLogic.NhanSu
 {
-//    public class MyConvert
-//    {
-//#region Convert To Entities
-//        public static NhanVien ToNhanVien(Tbl_NhanVien record)
-//        {
-//            return new NhanVien(record.maNV, record.hoTen, record.ngaySinh, record.ngayVaoLam, record.soCMND, record.ngayCapCMND,
-//                record.gioiTinh, record.email, record.soDT, record.diaChi, record.trangThai.Value);
-//        }
+    public class myconvert
+    {
+        #region convert to entities
+        public static NhanVien tonhanvien(Tbl_NhanVien record)
+        {
+            return new NhanVien(record.maNV, record.hoTen, record.ngaySinh, record.ngayVaoLam, record.soCMND, record.ngayCapCMND,
+                record.gioiTinh, record.email, record.soDT, record.diaChi, record.trangThai.Value);
+        }
 
-//        //public static PhongBan ToPhongBan(Tbl_PhongBan record)
-//        //{
-//        //    return new PhongBan(record.maPB, record.tenPB, record.diaDiem, record.soDT);
-//        //}
+        public static PhongBan tophongban(Tbl_PhongBan record)
+        {
+            return new PhongBan(record.maPB, record.tenPB, record.diaDiem, record.soDT);
+        }
 
-//        //public static ChucVu ToChucVu(Tbl_ChucVu record)
-//        //{
-//        //    return new ChucVu(record.maChucVu, record.tenCV, record.heSo, record.ghiChu);
-//        //}
-//        public static PhongBan ToPhongBan(Tbl_PhongBan record)
-//        {
-//            return new PhongBan(record.maPB, record.tenPB, record.diaDiem, record.soDT);
-//        }
+        public static ChucVu tochucvu(Tbl_ChucVu record)
+        {
+            return new ChucVu(record.maChucVu, record.tenCV, record.heSo, record.ghiChu);
+        }
 
-//        public static ChucVu ToChucVu(Tbl_ChucVu record)
-//        {
-//            return new ChucVu(record.maChucVu, record.tenCV, record.heSo, record.ghiChu);
-//        }
+        public static KyLuat tokyluat(Tbl_KyLuatNV record)
+        {
+            return new KyLuat(record.maKyLuat, record.hinhThucKL, record.ghiChu);
+        }
+        public static DaoTao todaotao(Tbl_DaoTaoNV record)
+        {
+            return new DaoTao(record.maKhoaDT, record.tenKhoaDT, record.ngayBD, record.ngayKT, record.diaDiem, record.hinhthucDT, (decimal)record.chiPhi);
+        }
+        #endregion
 
-//        //public static KyLuat ToKyLuat(Tbl_KyLuatNV record)
-//        //{
-//        //    return new KyLuat(record.maKyLuat, record.hinhThucKL, record.ghiChu);
-//        //}
-//        //public static DaoTao ToDaoTao(Tbl_DaoTaoNV record)
-//        //{
-//        //    return new DaoTao(record.maKhoaDT, record.tenKhoaDT, record.ngayBD, record.ngayKT, record.diaDiem, record.hinhthucDT, (decimal)record.chiPhi);
-//        //}
+        #region convert to objects of database
+        public static Tbl_NhanVien totbl_nhanvien(NhanVien nhanvien)
+        {
+            Tbl_NhanVien record = new Tbl_NhanVien();
 
-//        public static KyLuat ToKyLuat(Tbl_KyLuatNV record)
-//        {
-//            return new KyLuat(record.maKyLuat, record.hinhThucKL, record.ghiChu);
-//        }
-//        public static DaoTao ToDaoTao(Tbl_DaoTaoNV record)
-//        {
-//            return new DaoTao(record.maKhoaDT, record.tenKhoaDT, record.ngayBD, record.ngayKT, record.diaDiem, record.hinhthucDT, (decimal)record.chiPhi);
-//        }
-//#endregion
+            record.maNV = nhanvien.MaNV;
+            record.hoTen = nhanvien.HoTen;
+            record.ngaySinh = nhanvien.NgaySinh;
+            record.ngayVaoLam = nhanvien.NgayVaoLam;
+            record.soCMND = nhanvien.SoCMND;
+            record.ngayCapCMND = nhanvien.NgayCapCMND;
+            record.gioiTinh = nhanvien.GioiTinh;
+            record.email = nhanvien.Email;
+            record.soDT = nhanvien.SDT;
+            record.diaChi = nhanvien.DiaChi;
+            record.trangThai = nhanvien.TrangThai;
 
-//#region Convert To Objects Of Database
-//        public static Tbl_NhanVien ToTbl_NhanVien(NhanVien nhanvien)
-//        {
-//            Tbl_NhanVien record = new Tbl_NhanVien();
+            return record;
+        }
 
-//            record.maNV = nhanvien.MaNV;
-//            record.hoTen = nhanvien.HoTen;
-//            record.ngaySinh = nhanvien.NgaySinh;
-//            record.ngayVaoLam = nhanvien.NgayVaoLam;
-//            record.soCMND = nhanvien.SoCMND;
-//            record.ngayCapCMND = nhanvien.NgayCapCMND;
-//            record.gioiTinh = nhanvien.GioiTinh;
-//            record.email = nhanvien.Email;
-//            record.soDT = nhanvien.SDT;
-//            record.diaChi = nhanvien.DiaChi;
-//            record.trangThai = nhanvien.TrangThai;
+        public static Tbl_PhongBan totbl_phongban(PhongBan phongban)
+        {
+            Tbl_PhongBan record = new Tbl_PhongBan();
+            record.maPB = phongban.MaPB;
+            record.tenPB = phongban.TenPB;
+            record.diaDiem = phongban.DiaDiem;
+            record.soDT = phongban.Sdt;
 
-//            return record;
-//        }
+            return record;
+        }
 
-//        //public static Tbl_PhongBan toTbl_PhongBan(PhongBan phongban)
-//        //{
-//        //    Tbl_PhongBan record = new Tbl_PhongBan();
-//        //    record.maPB = phongban.MaPB;
-//        //    record.tenPB = phongban.TenPB;
-//        //    record.diaDiem = phongban.DiaDiem;
-//        //    record.soDT = phongban.Sdt;
+        public static Tbl_ChucVu totbl_chucvu(ChucVu chucvu)
+        {
+            Tbl_ChucVu record = new Tbl_ChucVu();
+            record.maChucVu = chucvu.MaCV;
+            record.tenCV = chucvu.TenCV;
+            record.heSo = chucvu.HeSo;
+            record.ghiChu = chucvu.GhiChu;
 
-//        //    return record;
-//        //}
+            return record;
+        }
 
-//        //public static Tbl_ChucVu toTbl_ChucVu(ChucVu chucvu)
-//        //{
-//        //    Tbl_ChucVu record = new Tbl_ChucVu();
-//        //    record.maChucVu = chucvu.MaCV;
-//        //    record.tenCV = chucvu.TenCV;
-//        //    record.heSo = chucvu.HeSo;
-//        //    record.ghiChu = chucvu.GhiChu;
+        public static Tbl_KyLuatNV totbl_kyluat(KyLuat kyluat)
+        {
+            Tbl_KyLuatNV record = new Tbl_KyLuatNV();
+            record.maKyLuat = kyluat.MaKL;
+            record.hinhThucKL = kyluat.HinhThucKL;
+            record.ghiChu = kyluat.GhiChu;
+            return record;
+        }
 
-//        //    return record;
-//        //}
+        public static Tbl_DaoTaoNV totbl_daotao(DaoTao daotao)
+        {
+            Tbl_DaoTaoNV record = new Tbl_DaoTaoNV();
+            record.maKhoaDT = daotao.MaKhoaDT;
+            record.tenKhoaDT = daotao.TenKhoaDT;
+            record.ngayBD = daotao.NgayBD;
+            record.ngayKT = daotao.NgayKT;
+            record.diaDiem = daotao.Diadiem;
+            record.hinhthucDT = daotao.HinhThucDT;
+            record.chiPhi = record.chiPhi;
+            return record;
+        }
+        #endregion
 
-//        public static Tbl_PhongBan toTbl_PhongBan(PhongBan phongban)
-//        {
-//            Tbl_PhongBan record = new Tbl_PhongBan();
-//            record.maPB = phongban.MaPB;
-//            record.tenPB = phongban.TenPB;
-//            record.diaDiem = phongban.DiaDiem;
-//            record.soDT = phongban.Sdt;
-
-//            return record;
-//        }
-
-//        public static Tbl_ChucVu toTbl_ChucVu(ChucVu chucvu)
-//        {
-//            Tbl_ChucVu record = new Tbl_ChucVu();
-//            record.maChucVu = chucvu.MaCV;
-//            record.tenCV = chucvu.TenCV;
-//            record.heSo = chucvu.HeSo;
-//            record.ghiChu = chucvu.GhiChu;
-
-//            return record;
-//        }
-
-//        public static Tbl_KyLuatNV toTbl_KyLuat(KyLuat kyluat)
-//        {
-//            Tbl_KyLuatNV record = new Tbl_KyLuatNV();
-//            record.maKyLuat = kyluat.MaKL;
-//            record.hinhThucKL = kyluat.HinhThucKL;
-//            record.ghiChu = kyluat.GhiChu;
-//            return record;
-//        }
-
-//        public static Tbl_DaoTaoNV toTbl_DaoTao(DaoTao daotao)
-//        {
-//            Tbl_DaoTaoNV record = new Tbl_DaoTaoNV();
-//            record.maKhoaDT = daotao.MaKhoaDT;
-//            record.tenKhoaDT = daotao.TenKhoaDT;
-//            record.ngayBD = daotao.NgayBD;
-//            record.ngayKT = daotao.NgayKT;
-//            record.diaDiem = daotao.Diadiem;
-//            record.hinhthucDT = daotao.HinhThucDT;
-//            record.chiPhi = record.chiPhi;
-//            return record;
-//        }
-// #endregion
-
-//    }
+    }
 }
--- SRPHR_Solution/BusinessLogic/NhanSu/PhongBanBLL.cs
+++ SRPHR_Solution/BusinessLogic/NhanSu/PhongBanBLL.cs
@@ -10,82 +10,14 @@ namespace BusinessLogic.NhanSu
 {
     public class PhongBanBLL
     {
-        ////SRPHRDataContext DB = new SRPHRDataContext();
-        ////public List<PhongBan> GetAllPhongBan()
-        ////{
-        ////    List<PhongBan> phongBans = new List<PhongBan>();
-        ////    PhongBan tempPB;
-        ////    foreach(Tbl_PhongBan record in DB.Tbl_PhongBans)
-        ////    {
-        ////        tempPB = MyConvert.ToPhongBan(record);
-        ////        phongBans.Add(tempPB);
-        ////    }
-
-        ////    return phongBans;
-        ////}
-
-        ////public bool Add(PhongBan newPB)
-        ////{
-        ////    try
-        ////    {
-        ////        Tbl_PhongBan newRecord = MyConvert.toTbl_PhongBan(newPB);
-        ////        DB.Tbl_PhongBans.InsertOnSubmit(newRecord);
-        ////        DB.SubmitChanges();
-
-        ////        return true;
-        ////    }
-        ////    catch
-        ////    {
-        ////        return false;
-        ////    }
-        ////}
-        ////public bool Delete(string deleteID)
-        ////{
-        ////    try
-        ////    {
-        ////        Tbl_PhongBan deleteRecord = DB.Tbl_PhongBans.Single(record => record.maPB == deleteID);
-        ////        DB.Tbl_PhongBans.DeleteOnSubmit(deleteRecord);
-        ////        DB.SubmitChanges();
-
-        ////        return true;
-        ////    }
-        ////    catch
-        ////    {
-        ////        return false;
-        ////    }
-        ////}
-
-        ////public bool UpDate(PhongBan updatePB)
-        ////{
-        ////    try
-        ////    {
-        ////        Tbl_PhongBan updateRecord = DB.Tbl_PhongBans.Single(record => record.maPB == updatePB.MaPB);
-        ////        updateRecord.maPB = updatePB.MaPB;
-        ////        updateRecord.tenPB = updatePB.TenPB;
-        ////        updateRecord.diaDiem = updatePB.DiaDiem;
-        ////        updateRecord.soDT = updatePB.Sdt;
-
-        ////        DB.SubmitChanges();
-
-        ////        return true;
-
-        ////    }
-        ////    catch
-        ////    {
-        ////        return false;
-        ////    }
-        ////}
-
-        public class PhongBanBLL
-        {
-            SRPHRDataContext DB = new SRPHRDataContext();
+        /*  SRPHRDataContext DB = new SRPHRDataContext();
             public List<PhongBan> GetAllPhongBan()
             {
                 List<PhongBan> phongBans = new List<PhongBan>();
                 PhongBan tempPB;
                 foreach (Tbl_PhongBan record in DB.Tbl_PhongBans)
                 {
-                    tempPB = MyConvert.ToPhongBan(record);
+                    tempPB = myconvert.tophongban(record);
                     phongBans.Add(tempPB);
                 }
 
@@ -93,10 +25,75 @@ namespace BusinessLogic.NhanSu
             }
 
             public bool Add(PhongBan newPB)
+            { 
+                try
+                {
+                    Tbl_PhongBan newRecord = myconvert.totbl_phongban(newPB);
+                    DB.Tbl_PhongBans.InsertOnSubmit(newRecord);
+                    DB.SubmitChanges();
+
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            public bool Delete(string deleteID)
             {
                 try
                 {
-                    Tbl_PhongBan newRecord = MyConvert.toTbl_PhongBan(newPB);
+                    Tbl_PhongBan deleteRecord = DB.Tbl_PhongBans.Single(record => record.maPB == deleteID);
+                    DB.Tbl_PhongBans.DeleteOnSubmit(deleteRecord);
+                    DB.SubmitChanges();
+
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            public bool UpDate(PhongBan updatePB)
+            {
+                try
+                {
+                    Tbl_PhongBan updateRecord = DB.Tbl_PhongBans.Single(record => record.maPB == updatePB.MaPB);
+                    updateRecord.maPB = updatePB.MaPB;
+                    updateRecord.tenPB = updatePB.TenPB;
+                    updateRecord.diaDiem = updatePB.DiaDiem;
+                    updateRecord.soDT = updatePB.Sdt;
+
+                    DB.SubmitChanges();
+
+                    return true;
+
+                }
+                catch
+                {
+                    return false;
+                }
+            }*/
+            SRPHRDataContext DB = new SRPHRDataContext();
+            public List<PhongBan> GetAllPhongBan()
+            {
+                List<PhongBan> phongBans = new List<PhongBan>();
+                PhongBan tempPB;
+                foreach (Tbl_PhongBan record in DB.Tbl_PhongBans)
+                {
+                    tempPB = myconvert.tophongban(record);
+                    phongBans.Add(tempPB);
+                }
+
+                return phongBans;
+            }
+
+            public bool Add(PhongBan newPB)
+            { 
+                try
+                {
+                    Tbl_PhongBan newRecord = myconvert.totbl_phongban(newPB);
                     DB.Tbl_PhongBans.InsertOnSubmit(newRecord);
                     DB.SubmitChanges();
 
@@ -144,5 +141,4 @@ namespace BusinessLogic.NhanSu
                 }
             }
         }
-    }
 }
