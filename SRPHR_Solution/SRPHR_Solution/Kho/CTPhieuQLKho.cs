3e580c4aa7083305edf9cbbd4e54869c7096bb80
sonnvit <sonnv.vnu@gmail.com>
2016-11-06 12:05:37 +0700

    update form PQLKho and CTPhieuQLKho

---
+++ SRPHR_Solution/SRPHR_Solution/Kho/CTPhieuQLKho.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entities.Kho;
+using BusinessLogic.Kho;
+
+namespace SRPHR_Solution.Kho
+{
+    public partial class CTPhieuQLKho : Form
+    {
+        List<eCTQLKho> lst = new List<eCTQLKho>();
+        CTPQLKho_BLL ctqlkBLL = new CTPQLKho_BLL();
+        BindingSource bds = new BindingSource();
+
+        public CTPhieuQLKho()
+        {
+            InitializeComponent();
+        }
+        public void loaddata()
+        {
+            lst = ctqlkBLL.getAllCTPhieuQLKho();
+            bds.DataSource = lst;
+            dataCTPQLK.DataSource = bds;
+        }
+
+        private void CTPhieuQLKho_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            eCTQLKho ect = new eCTQLKho();
+            ect._maPhieuQL = txtmaphieu.Text;
+            ect._sott = txtstt.Text;
+            ect._masp = txtmasp.Text;
+            ect._tensp = txttensp.Text;
+            ect._soluongql = txtsoluong.Text;
+            ect._donvi = txtdonvi.Text;
+            bool kq = ctqlkBLL.AddAllCTPQLKho(ect);
+            if (kq == false)
+            {
+                MessageBox.Show("Không có mã phiếu để thêm!", "Thông báo!");
+
+            }
+            else
+            {
+                MessageBox.Show("Thêm thành công!", "Thông báo!");
+                dataCTPQLK.DataSource = ctqlkBLL.getAllCTPhieuQLKho();
+            }
+
+
+
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            eCTQLKho ect = new eCTQLKho();
+            ect._maPhieuQL = txtmaphieu.Text;
+            ect._sott = txtstt.Text;
+            ect._masp = txtmasp.Text;
+            ect._tensp = txttensp.Text;
+            ect._soluongql = txtsoluong.Text;
+            ect._donvi = txtdonvi.Text;
+            bool kq = ctqlkBLL.UpdateCTPQLKho(ect);
+            if (kq == false)
+            {
+                MessageBox.Show("Không có mã để sửa", "Thông báo!");
+            }
+            else
+            {
+                MessageBox.Show("Sửa thành công!", "Thông báo!");
+                dataCTPQLK.DataSource = ctqlkBLL.getAllCTPhieuQLKho();
+            }
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            eCTQLKho ect = new eCTQLKho();
+            ect._maPhieuQL = txtmaphieu.Text;
+            ect._sott = txtstt.Text;
+            ect._masp = txtmasp.Text;
+            ect._tensp = txttensp.Text;
+            ect._soluongql = txtsoluong.Text;
+            ect._donvi = txtdonvi.Text;
+            bool kq = ctqlkBLL.DeleteCTPhieuQLK(ect);
+            if (kq == false)
+            {
+                MessageBox.Show("Không có mã để Xóa", "Thông báo!");
+            }
+            else
+            {
+                MessageBox.Show("Xóa thành công!", "Thông báo!");
+                dataCTPQLK.DataSource = ctqlkBLL.getAllCTPhieuQLKho();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ SRPHR_Solution/SRPHR_Solution/Kho/PQLKho.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entities.Kho;
+using BusinessLogic.Kho;
+
+namespace SRPHR_Solution.Kho
+{
+    public partial class PQLKho : Form
+    {
+        List<ePhieuQLiKho> lst = new List<ePhieuQLiKho>();
+        PhieuQLKho_BLL pqlkBLL = new PhieuQLKho_BLL();
+        BindingSource bds = new BindingSource();
+        public PQLKho()
+        {
+            InitializeComponent();
+        }
+        public void loaddata()
+        {
+            lst = pqlkBLL.getAllPQLKho();
+            bds.DataSource = lst;
+            dataPQLK.DataSource = bds;
+
+        }
+
+        private void PQLKho_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            ePhieuQLiKho qlk = new ePhieuQLiKho();
+            qlk._maPhieuQli = txtmphieu.Text;
+            qlk._ngayLapQLi = txtngaylap.Text;
+            qlk._maNV = txtmanv.Text;
+            qlk._maKho = txtmakho.Text;
+            qlk._tinhTrang = txttinhtrang.Text;
+            bool kq = pqlkBLL.AddAllPhieuQLK(qlk);
+            if (kq == false)
+            {
+                MessageBox.Show("Không có mã để thêm", "Thông báo!");
+
+            }
+            else
+            {
+                MessageBox.Show("Thêm thành công", "Thông báo!");
+                dataPQLK.DataSource = pqlkBLL.getAllPQLKho();
+            }
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            ePhieuQLiKho qlk = new ePhieuQLiKho();
+            qlk._maPhieuQli = txtmphieu.Text;
+            qlk._ngayLapQLi = txtngaylap.Text;
+            qlk._maNV = txtmanv.Text;
+            qlk._maKho = txtmakho.Text;
+            qlk._tinhTrang = txttinhtrang.Text;
+            bool kq = pqlkBLL.UpDatePhieuQLK(qlk);
+            if (kq == false)
+            {
+                MessageBox.Show("Không có mã để sửa!", "Thông báo!");
+
+            }
+            else
+            {
+                MessageBox.Show("Sửa thành công!", "Thông báo!");
+                dataPQLK.DataSource = pqlkBLL.getAllPQLKho();
+            }
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            ePhieuQLiKho qlk = new ePhieuQLiKho();
+            qlk._maPhieuQli = txtmphieu.Text;
+            qlk._ngayLapQLi = txtngaylap.Text;
+            qlk._maNV = txtmanv.Text;
+            qlk._maKho = txtmakho.Text;
+            qlk._tinhTrang = txttinhtrang.Text;
+            bool kq = pqlkBLL.DeletePhieuQLK(qlk);
+            if (kq == false)
+            {
+                MessageBox.Show("Không có mã để xóa!", "Thông báo!");
+
+            }
+            else
+            {
+                MessageBox.Show("Xóa thành công!", "Thông báo!");
+                dataPQLK.DataSource = pqlkBLL.getAllPQLKho();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
