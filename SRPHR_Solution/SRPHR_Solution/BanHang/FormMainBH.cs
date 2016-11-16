0e9e9394fb9100471c9dc0ee3bee008bef2dfe48
keal0129 <keal0129@gmail.com>
2016-11-16 12:34:58 +0700

    Update PQ BanHang

--- SRPHR_Solution/SRPHR_Solution/BanHang/FormMainBH.cs
+++ SRPHR_Solution/SRPHR_Solution/BanHang/FormMainBH.cs
@@ -7,19 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SRPHR_Solution.PhanQuyen;
 
 namespace SRPHR_Solution.BanHang
 {
     public partial class FormMainBH : Form
     {
+        public string PQMainBH;
         public FormMainBH()
         {
             InitializeComponent();
+            
         }
 
         private void FormMainBH_Load(object sender, EventArgs e)
-        { 
-
+        {
+            
         }
 
         private void btnhdgtgt_Click(object sender, EventArgs e)
@@ -31,7 +34,9 @@ namespace SRPHR_Solution.BanHang
             //show form báo cáo
             FormHDGTGT frmnew = new FormHDGTGT();
             frmnew.Activate();
+            frmnew.PQHDGTGT = PQMainBH;
             frmnew.ShowDialog();
+           
         }
 
         private void btnhdbanle_Click(object sender, EventArgs e)
@@ -43,7 +48,9 @@ namespace SRPHR_Solution.BanHang
             //show form báo cáo
             FormHDBanle frmnew = new FormHDBanle();
             frmnew.Activate();
+            frmnew.PQHDBL = PQMainBH;
             frmnew.ShowDialog();
         }
+        
     }
 }
